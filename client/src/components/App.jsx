1abb6f338ca5f2477bb2afd812b0f91e2705edf2
jim walter <jimwalter13090@gmail.com>
2020-03-04 19:25:32 -0700

    webpack, index and 1st component working

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      groceries: []
+    }
+  }
+
+  render() {
+    return <div>HELLO</div>
+  }
+}
+
+
+export default App;
